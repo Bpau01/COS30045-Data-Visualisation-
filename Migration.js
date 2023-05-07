c8199f6814a3ed66c0de2e7e5572ec9c06773988
Bryan <pau.bryan@yahoo.com>
2023-05-07 16:49:23 +1000

    Updates to visualisation 1 & 2

--- Migration.js
+++ Migration.js
@@ -7,10 +7,32 @@ function drawWorldMap(year) {
     .scaleQuantize()
     .range(["#feedde", "#fdbe85", "#fd8d3c", "#e6550d", "#a63603"]);
 
+  var nameMap = {
+    "Western Sahara": "W. Sahara",
+    USA: "United States of America",
+    "Congo, Dem Rep": "Dem. Rep. Congo",
+    "Dominican Rep": "Dominican Rep.",
+    "Falkland Is": "Falkland Is.",
+    "Cote d'Ivoire": "Côte d'Ivoire",
+    "Cent Africa Rep": "Central African Rep.",
+    "Congo, Rep": "Congo",
+    "Equator Guinea": "Eq. Guinea",
+    Eswatini: "eSwatini",
+    "Unit Arab Emir": "United Arab Emirates",
+    "Korea, North": "North Korea",
+    "Korea, South": "South Korea",
+    "Solomon Islands": "Solomon Is.",
+    "UK, CIs & IOM": "United Kingdom",
+    "Bosnia/Herzegov": "Bosnia and Herz.",
+    "North Macedonia": "Macedonia",
+    "Trinidad/Tobago": "Trinidad and Tobago",
+    "South Sudan": "S. Sudan",
+  };
+
   //Remove existing map
-  d3.select("#chart svg").remove();
+  d3.select("#chart1 svg").remove();
   var svg = d3
-    .select("#chart")
+    .select("#chart1")
     .append("svg")
     .attr("width", w)
     .attr("height", h)
@@ -27,7 +49,7 @@ function drawWorldMap(year) {
     "data1.csv",
     function (d) {
       return {
-        country: d.Country,
+        country: nameMap[d.Country] || d.Country,
         value: parseFloat(d[year].replace(/,/g, "")),
       };
     },
@@ -58,7 +80,9 @@ function drawWorldMap(year) {
           .attr("d", path)
           .attr("fill", function (world) {
             var value = world.properties.value;
-            console.log(world.properties.name);
+            if (value === undefined || value === null) {
+              console.log(world.properties.name);
+            }
             if (value >= 3000) {
               return color.range()[4];
             } else if (value >= 1000 && value < 3000) {
@@ -70,15 +94,112 @@ function drawWorldMap(year) {
             } else if (value < 200) {
               return color.range()[0];
             }
+          })
+          .on("mouseover", function () {
+            d3.select(this).attr("fill", "blue");
+          })
+          .on("mouseout", function () {
+            var value = d3.select(this).data()[0].properties.value;
+            if (value >= 3000) {
+              d3.select(this).attr("fill", color.range()[4]);
+            } else if (value >= 1000 && value < 3000) {
+              d3.select(this).attr("fill", color.range()[3]);
+            } else if (value >= 500 && value < 1000) {
+              d3.select(this).attr("fill", color.range()[2]);
+            } else if (value >= 200 && value < 500) {
+              d3.select(this).attr("fill", color.range()[1]);
+            } else if (value < 200) {
+              d3.select(this).attr("fill", color.range()[0]);
+            }
           });
       });
     }
   );
 }
 
+function pieChart() {
+  // Define the initial data for the donut chart
+  const data = [100, 50, 30];
+
+  // Define the dimensions of the SVG element and the radius of the donut chart
+  const width = 400;
+  const height = 400;
+  const radius = Math.min(width, height) / 2 - 10;
+
+  // Define the color scale for the chart segments
+  const color = d3
+    .scaleOrdinal()
+    .domain(data.map((d, i) => i))
+    .range(["#4daf4a", "#377eb8", "#ff7f00"]);
+
+  // Define the arc generator for the chart segments
+  const arc = d3
+    .arc()
+    .innerRadius(radius * 0.5)
+    .outerRadius(radius * 0.8);
+
+  // Define the arc generator for the hover segments
+  const hoverArc = d3
+    .arc()
+    .innerRadius(radius * 0.7)
+    .outerRadius(radius * 0.9);
+
+  // Define the pie generator for the chart data
+  const pie = d3
+    .pie()
+    .sort(null)
+    .value((d) => d);
+
+  // Create the SVG element and add a group for the chart
+  const svg = d3
+    .select("#chart2")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+  // Add the segments to the chart
+  const segments = svg
+    .selectAll("path")
+    .data(pie(data))
+    .enter()
+    .append("path")
+    .attr("d", arc)
+    .attr("fill", (d, i) => color(i))
+    .on("mouseover", function (d) {
+      // Get the data for the hover segments
+      const hoverData = [70, 20, 10];
+
+      // Remove the old hover segments
+      svg.selectAll(".hover-segment").remove();
+
+      // Add the new hover segments
+      svg
+        .selectAll(".hover-segment")
+        .data(pie(hoverData))
+        .enter()
+        .append("path")
+        .attr("class", "hover-segment")
+        .attr("d", hoverArc)
+        .attr("fill", (d, i) => color(i))
+        .attr("stroke", "white")
+        .attr("stroke-width", 2)
+        .style("opacity", 0.7)
+        .each(function (d) {
+          // Store the old data for the hover segment
+          this._current = d;
+        });
+    })
+    .on("mouseout", function (d) {
+      // Remove the hover segments
+      svg.selectAll(".hover-segment").remove();
+    });
+}
+
 function main() {
   drawWorldMap("2004-05");
-
+  pieChart();
   var slider = d3.select("#slider");
   // Get value based on slider
   slider.on("input", function () {
